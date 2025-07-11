6747a0411a4fd761776c741bab21585bd2a09a1f
tunalpha <ildattero.it@gmail.com>
2025-07-12 01:18:36 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2433,6 +2433,7 @@ function App() {
                   type="password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
+                  autoComplete="new-password"
                   className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                   placeholder="Inserisci la password admin"
                   required
